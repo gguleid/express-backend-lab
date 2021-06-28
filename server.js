2bc795779c67ec4463d3340b3c937cc3d190d48d
gguleid <gguleid@gmail.com>
2021-06-28 15:32:29 -0400

    backend is done

---
+++ server.js
@@ -0,0 +1,37 @@
+// Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Import Json files
+const projects = require('./projects.json');
+const about = require('./about.json');
+
+
+// Create the app object
+const app = express();
+
+// Middleware
+app.use(cors());
+
+
+// Routes
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Retreieving projects
+app.get('/projects', (req, res) => {
+    res.json(projects);
+});
+
+// Retreiving about
+app.get('/about', (req, res) => {
+    res.json(about);
+})
+
+
+// Listener
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
